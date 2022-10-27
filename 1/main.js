76952fd6a76b4fdcfe1d7eedeca3fe665ec5308e
y2610-11 <andrew.vi.2610@gmail.com>
2022-10-27 15:22:58 +0200

    create carpets

---
+++ 1/main.js
@@ -0,0 +1,20 @@
+function sumNum(objeto){
+    let numeros_de_pares = objeto.filter(item => item/2 == 0)
+console.log(numeros_de_pares)
+}
+let numeros = [1,2,3,4,5,6,7,8,9,0]
+console.log(sumNum(numeros))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
