659c15a7b10bba037971bd92b9f0e252b8f6f7c6
y2610-11 <andrew.vi.2610@gmail.com>
2022-10-27 16:19:09 +0200

    another one

--- 1/main.js
+++ 1/main.js
@@ -1,20 +1,14 @@
-function sumNum(objeto){
-    let numeros_de_pares = objeto.filter(item => item/2 == 0)
-console.log(numeros_de_pares)
-}
+
 let numeros = [1,2,3,4,5,6,7,8,9,0]
+
+function sumNum(objeto){
+let even = objeto.filter(item => item % 2 == 0)
+console.log(even)
+let suma = 0
+for(x=0;x<even.length;x++){
+    suma += even[x]
+    
+}
+return suma
+}
 console.log(sumNum(numeros))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ 2/main.js
@@ -0,0 +1,10 @@
+let numeros = [1,2,3,4,5,6,7,8,9]
+
+function reverseArray(objeto){
+    console.log(objeto)
+let arrayReverse = objeto.reverse()
+return arrayReverse
+
+}
+
+console.log(reverseArray(numeros));
